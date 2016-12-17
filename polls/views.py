8cb4bb2e129d8b2525d06932410072a6df5ff74a
Sherwood Callaway <shcallaway@gmail.com>
2016-12-17 15:09:46 -0500

    Added polls detail view and URL namespacing

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 # from django.http import HttpResponse
 # from django.template import loader
+# from django.http import Http404
 
 from .models import Question, Choice
 
@@ -18,8 +19,17 @@ def index(request):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# Replace verbose way of getting object or 404 with shortcut
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
